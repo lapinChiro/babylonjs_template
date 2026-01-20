fc18e163760c75b8bed98fb7925151693936e6e9
noda_taro <noda_taro@uniquevision.co.jp>
2026-01-20 11:01:50 +0900

    feat: vue_hono_postgres のスタイル除去

--- templates/vue_hono_postgres/apps/frontend/src/main.ts
+++ templates/vue_hono_postgres/apps/frontend/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { setupVeeValidate } from './utils/vee-validate-config'
 import router from './router'
-import './style.css'
 import App from './App.vue'
 
 // VeeValidateのグローバル設定
